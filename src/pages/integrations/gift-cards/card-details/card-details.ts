9a96ccf678b0bcc452740aff2a38bc0d21a330a4
Marty Alcala <marty@bitpay.com>
2018-08-03 18:22:31 -0400

    bind to real data

--- src/pages/integrations/gift-cards/card-details/card-details.ts
+++ src/pages/integrations/gift-cards/card-details/card-details.ts
@@ -1,7 +1,24 @@
 import { Component } from '@angular/core';
+import { NavParams } from 'ionic-angular';
+
+export interface GiftCard {
+  claimCode: string;
+  amount: number;
+  currency: string;
+  date: number;
+}
 
 @Component({
   selector: 'card-details-page',
   templateUrl: 'card-details.html'
 })
-export class CardDetailsPage {}
+export class CardDetailsPage {
+  public card: GiftCard;
+
+  constructor(private navParams: NavParams) {}
+
+  ngOnInit() {
+    this.card = this.navParams.get('card');
+    console.log('this.card', this.card);
+  }
+}
