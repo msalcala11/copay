9444c326e2801469f526986685a8644e1a515062
Marty Alcala <marty@bitpay.com>
2018-08-08 13:38:22 -0400

    wire gift card buy page to amount page

--- src/pages/integrations/gift-cards/buy-card/buy-card.ts
+++ src/pages/integrations/gift-cards/buy-card/buy-card.ts
@@ -1,16 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AmazonProvider } from '../../../../providers/amazon/amazon';
+import { OnGoingProcessProvider } from '../../../../providers/on-going-process/on-going-process';
+import { AmountPage } from '../../../send/amount/amount';
 
 @Component({
   selector: 'buy-card-page',
   templateUrl: 'buy-card.html'
 })
 export class BuyCardPage {
-  constructor(private nav: NavController) {}
+  constructor(
+    private amazonProvider: AmazonProvider,
+    private nav: NavController,
+    private onGoingProcessProvider: OnGoingProcessProvider
+  ) {}
+
+  ngOnInit() {
+    this.initAmazon();
+  }
 
   cancel() {
     this.nav.pop();
   }
 
-  enterAmount() {}
+  private async initAmazon(): Promise<any> {
+    if (!this.amazonProvider.currency) {
+      this.onGoingProcessProvider.set('');
+      await this.amazonProvider.setCurrencyByLocation();
+      this.onGoingProcessProvider.clear();
+    }
+  }
+
+  enterAmount() {
+    this.nav.push(AmountPage, {
+      nextPage: 'BuyAmazonPage',
+      currency: this.amazonProvider.currency,
+      fixedUnit: true,
+      onlyIntegers: this.amazonProvider.onlyIntegers
+    });
+  }
 }
