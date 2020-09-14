55c03df7780490e20e63d56b35624fadbdb089cd
Marty Alcala <marty@bitpay.com>
2020-09-14 12:27:24 -0600

    better styling of create payId form

--- src/pages/pay-id/create-pay-id/create-pay-id.ts
+++ src/pages/pay-id/create-pay-id/create-pay-id.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavController } from 'ionic-angular';
+import { InfoSheetComponent } from '../../../components/info-sheet/info-sheet';
 import { ActionSheetProvider } from '../../../providers';
 import { ReceivingWalletsPage } from '../receiving-wallets/receiving-wallets';
 
@@ -10,11 +11,14 @@ import { ReceivingWalletsPage } from '../receiving-wallets/receiving-wallets';
 })
 export class CreatePayIdPage {
   public createForm: FormGroup;
+  public hasError: boolean = false;
+  public infoSheet: InfoSheetComponent;
 
   constructor(
     private fb: FormBuilder,
     private nav: NavController,
-    private actionSheetProvider: ActionSheetProvider
+    private actionSheetProvider: ActionSheetProvider,
+    private zone: NgZone
   ) {
     this.createForm = this.fb.group({
       payId: ['', Validators.required]
@@ -23,12 +27,23 @@ export class CreatePayIdPage {
 
   createPayId() {
     console.log('create payId called');
-    this.nav.push(ReceivingWalletsPage);
+    console.log('this.nav', this.nav, ReceivingWalletsPage);
+    // this.nav.push(ReceivingWalletsPage);
+    this.showTakenActionSheet();
+  }
+
+  onChange() {
+    this.zone.run(() => {
+      this.hasError = false;
+      this.infoSheet && this.infoSheet.dismiss();
+    });
   }
 
   showTakenActionSheet() {
-    this.actionSheetProvider
-      .createInfoSheet('pay-id-taken', { payId: this.createForm.value.payId })
-      .present();
+    this.hasError = true;
+    this.infoSheet = this.actionSheetProvider.createInfoSheet('pay-id-taken', {
+      payId: this.createForm.value.payId
+    });
+    this.infoSheet.present();
   }
 }
