cdb4a92c109425481dc0e518f76987b0651e0c28
Marty Alcala <marty@bitpay.com>
2020-09-28 14:13:17 -0700

    style code box

--- src/pages/pay-id/verify/verify.ts
+++ src/pages/pay-id/verify/verify.ts
@@ -5,5 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: 'verify.html'
 })
 export class VerifyPayIdPage {
-  constructor() {}
+  code: string[] = [
+    '23423',
+    '59872',
+    '39712',
+    '00510',
+    '23423',
+    '59872',
+    '39712',
+    '00510',
+    '23423',
+    '59872',
+    '39712',
+    '00510'
+  ];
+  codeRows: string[][];
+  constructor() {
+    this.codeRows = chunkify(this.code, 4);
+    console.log('codeRows', this.codeRows);
+  }
+}
+
+function chunkify(inputArray: string[], perChunk: number): string[][] {
+  return inputArray.reduce((all, one, i) => {
+    const ch = Math.floor(i / perChunk);
+    all[ch] = [].concat(all[ch] || [], one);
+    return all;
+  }, []);
 }
